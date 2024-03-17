ab0ae9172849c094b532da4bd714dc91e9ab2d9f
guse95 <154426320+guse95@users.noreply.github.com>
2024-03-17 16:28:18 +0300

    Психанул master.cpp

--- master.cpp
+++ master.cpp
@@ -15,11 +15,51 @@ using namespace std;
 using graph = vector<vector<int>>;
 using vi = vector<int>;
 using ll = long long;
-using str = char;
 
-const int N = 2e5;
-graph g;
-vi tin(N), tout(N);
+#define all(x) x.begin(), x.end()
+
+using ll = long long;
+
+bool kun(int u, const graph& g, vector<int>& mt, vector<int>& vis) {
+    if (vis[u]) {
+        return false;
+    }
+    vis[u] = true;
+    for (int v : g[u]) {
+        if (mt[v] == -1 || kun(mt[v], g, mt, vis)) {
+            mt[v] = u;
+            return true;
+        }
+    }
+    return false;
+}
+
+const int black = 1;
+const int green = 2;
+const int red = 3;
+
+bool dfs(int u, int prev, const graph& g, vi& colour) {
+    if (colour[u] != black) {
+        if (colour[u] != colour[prev]) {
+            return true;
+        }
+        return false;
+    }
+    if (colour[prev] == green) {
+        colour[u] = red;
+    } else {
+        colour[u] = green;
+    }
+    for (int v : g[u]) {
+        if (v == prev) {
+            continue;
+        }
+        bool found = dfs(v, u, g, colour);
+        if (!found) {
+            return false;
+        }
+    }
+}
 
 void solve() {
     int n, m;
@@ -30,13 +70,24 @@ void solve() {
         cin >> u >> v;
         --u;
         --v;
+        if (u == v) {
+            cout << -1;
+            return;
+        }
         g[u].push_back(v);
+    }
+    vi colour(n, black);
+    if (!dfs(0, 0, g, colour)) {
+        cout << -1;
+        return;
     }
     vector<int> mt(n, -1), vis(n);
     int ans = 0;
     for (int i = 0; i < n; ++i) {
-        vis.assign(n, 0);
-        ans += kun(i, g, mt, vis);
+        if (colour[i] == green) {
+            vis.assign(n, 0);
+            ans += kun(i, g, mt, vis);
+        }
     }
     cout << ans << '\n';
     for (int i = 0; i < n; ++i) {
@@ -49,45 +100,6 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-    int n, q, cnt = 0, ind = 0, n1 = 0;
-    cin >> n >> q;
-    vector<ll> A(n);
-    vector<ll> kex(q);
-    vector<ll> kex_s(q);
-    map<int, int> ans;
-    for (int i = 0; i < n; ++i) {
-        cin >> A[i];
-    }
-    for (int i = 0; i < q; ++i) {
-        cin >> kex[i];
-        kex_s[i] = kex[i];
-    }
-    sort(all(kex_s));
-    for (ll i = 0; i < (1e9 + 1); ++i) {
-        if (ind < n) {
-            if (i != A[ind]) {
-                cnt += 1;
-            }
-            else {
-                ind += 1;
-            }
-        }
-        else {
-            cnt += 1;
-        }
-        while (cnt == kex[n1]) {
-            ans[kex[n1]] = i;
-            n1 += 1;
-            if (n1 > (q - 1)) {
-                break;
-            }
-        }
-        if (n1 > (q - 1)) {
-            break;
-        }
-    }
-    for (int i = 0; i < q; ++i) {
-        cout << ans[kex_s[i]] << ' ';
-    }
+    
+    solve();
 }
